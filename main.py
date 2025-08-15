e82602a2e1c2bf0db4bdf85a19ae1c1fbb509a3a
Teriso <den.xalturint@list.ru>
2025-08-15 13:48:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Токен телеграмм-бота
+# Токен телеграмм-бот
 TOKEN = ''
 
 # Выбор модели
@@ -182,6 +182,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
